6ca4f65373ea65ff8882dcf9993291826273ec31
Kristopher Cost <me@krisco.st>
2019-09-24 19:27:17 -0700

    adding CannonKeys Ortho60 keymaps, removing KeyLock on Preonic

--- keyboards/cannonkeys/ortho60/keymaps/Lavos/keymap.c
+++ keyboards/cannonkeys/ortho60/keymaps/Lavos/keymap.c
@@ -1,29 +1,14 @@
-/* Copyright 2015-2017 Jack Humbert
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include QMK_KEYBOARD_H
+extern keymap_config_t keymap_config;
 
-enum preonic_layers {
+enum _layers {
 	_BASE,
 	_BLUE,
 	_RED, 
 	_YELLOW
 };
 
-enum preonic_keycodes {
+enum _keycodes {
 	BASE = SAFE_RANGE,
 	BLUE,
 	RED,
@@ -32,15 +17,15 @@ enum preonic_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-[_BASE] = LAYOUT_preonic_grid( \
+[_BASE] = LAYOUT_ortho_5x12( \
 	GRAVE_ESC,	KC_EXLM,	KC_AT,		KC_HASH,	KC_DOLLAR,	KC_PERC,	KC_CIRC,	KC_AMPR,	KC_ASTR,	KC_MINUS,	KC_EQUAL,	KC_BSPC,\
 	KC_TAB,		KC_Q,		KC_W,		KC_E,		KC_R,		KC_T,		KC_Y,		KC_U,		KC_I,		KC_O,		KC_P,		KC_DEL,	\
-	_______,	KC_A,		KC_S,		KC_D,		KC_F,		KC_G,		KC_H,		KC_J,		KC_K,		KC_L,		KC_SCLN,	KC_PGUP,\
+	KC_LOCK,	KC_A,		KC_S,		KC_D,		KC_F,		KC_G,		KC_H,		KC_J,		KC_K,		KC_L,		KC_SCLN,	KC_PGUP,\
 	KC_LSFT,	KC_Z,		KC_X,		KC_C,		KC_V,		KC_B,		KC_N,		KC_M,		KC_COMM,	KC_DOT,		KC_SLSH,	KC_PGDOWN,\
 	KC_LCTL,	KC_LALT,	LCTL(KC_A),	MO(_RED),	MO(_YELLOW),	KC_SPC,		KC_ENT,		MO(_BLUE),	KC_LGUI,	_______,	RCTL(KC_A),	KC_RCTL\
 ),
 
-[_BLUE] = LAYOUT_preonic_grid( \
+[_BLUE] = LAYOUT_ortho_5x12( \
 	KC_F1,		KC_F2,		KC_F3,		KC_F4,		KC_F5,		KC_F6,		KC_F7,		KC_F8,		KC_F9,		KC_F10,		KC_F11,		KC_F12,	\
 	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,\
 	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,\
@@ -48,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______ \
 )
 ,
-[_RED] = LAYOUT_preonic_grid( \
+[_RED] = LAYOUT_ortho_5x12( \
 	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	KC_LPRN,	KC_RPRN,	KC_GRAVE,	KC_BSLASH,\
 	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	KC_LCBR,	KC_RCBR,	_______,	KC_INSERT,\
 	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	KC_LBRC,	KC_RBRC,	KC_QUOTE,	KC_HOME,\
@@ -56,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	KC_LEFT,	KC_DOWN,	KC_RIGHT\
 ),
 
-[_YELLOW] = LAYOUT_preonic_grid( \
+[_YELLOW] = LAYOUT_ortho_5x12( \
 	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	KC_P7,		KC_P8,		KC_P9,		_______,\
 	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	KC_P4,		KC_P5,		KC_P6,		_______,\
 	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	KC_P1,		KC_P2,		KC_P3,		KC_NLCK,\
